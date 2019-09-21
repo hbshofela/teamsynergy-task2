1d3af076255f667412099d77010682c783784d5a
hbshofela@gmail.com <Tommy123>
2019-09-21 14:22:59 +0100

    database connection

--- model/conn.php
+++ model/conn.php
@@ -1,8 +1,13 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$db="login";
+	//$servername = "localhost";
+	//$username = "root";
+	//$password = "";
+	//$db="login";
+
+	$servername = "tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+	$username = "icfnzdm2bah1l04t";
+	$password = "r7lwux1y2ls2cypf ";
+	$db="f3aht08s1ba49maw";
 	//created connection_aborted
 	$conn = new mysqli($servername, $username, $password, $db);
 	//check connection
